ed36ce29a12075559545750ccc24ca13095dbb99
Jaka Asa Baldan Ahmad <jakaasha@gmail.com>
2020-04-27 15:53:38 +0700

    Update binaryTree.cpp

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -11,9 +11,8 @@ struct Tree{
 
     Tree (T d){
         data = d;
-        kiri = kanan = NULL;
+        pangkal = kiri = kanan = NULL;
     }
-    void tambahCabang(T d);
 };
 /*--------          ./BATANG TUBUH BINARY TREE          --------*/
 
@@ -21,20 +20,111 @@ struct Tree{
 /*--------          SERBA-SERBI BINARY TREE          --------*/
 
     //-------- TINGGI BINARY TREE --------//
-    template <class T>
-    int tinggiBT(Tree<T>* tr, int x){
-        if(tr == NULL){
-            return x;
+        template <class T>
+        int tinggiBT(Tree<T>* tr, int x=0){
+            if(tr == NULL){
+                return x;
+            }
+            int a = tinggiBT(tr->kiri, x+1);
+            int b = tinggiBT(tr->kanan, x+1);
+            
+            return (a>b)?a: b;
         }
-        int a = tinggiBT(tr->kiri, x+1);
-        int b = tinggiBT(tr->kanan, x+1);
-        
-        return (a>b)?a: b;
-    }
     //-------- ./TINGGI BINARY TREE --------//
 
-    
+    //-------- LEVEL BINARY TREE --------//
+        template <class T>
+        int levelBT(Tree<T>* tr, T key){
+            Tree<T>* keyNode = searchNode(tr,key);
+            if(keyNode == NULL){
+                cout<<"Key tidak ditemukan!"<<endl;
+                return 0;
+            }
+            int level = 1;
+            while(keyNode->pangkal != NULL){
+                keyNode = keyNode->pangkal;
+                level++;
+            }
+            return level;
+        }
+    //-------- ./LEVEL BINARY TREE --------//
+
 /*--------          ./SERBA-SERBI BINARY TREE          --------*/
+
+
+/*--------          PENCARIAN BINARY TREE          --------*/
+
+    //-------- RETURN BOOLEAN  --------//
+        template <class T>
+        bool searchBool(Tree<T>* tr, T x){
+            if( tr == NULL){
+                return false;
+            }
+            if(tr->data == x){
+                return true;
+            }
+
+
+            return (searchBool(tr->kanan, x) || searchBool(tr->kiri, x));
+        }
+
+    //-------- ./RETURN BOOLEAN  --------//
+
+    //-------- RETURN NODE  --------//
+        template <class T>
+        Tree<T>* searchNode(Tree<T>* tr, T x){
+            if( tr == NULL){
+                return NULL;
+            }
+            if(tr->data == x){
+                return tr;
+            }
+
+            Tree<T>* a = searchNode(tr->kanan,x);
+            Tree<T>* b = searchNode(tr->kiri,x);
+            
+            if(a != NULL){
+                return a;
+            }
+            else if(b != NULL){
+                return b;
+            }
+            else{
+                return NULL;
+            }
+
+            
+        }
+
+    //-------- ./RETURN NODE  --------//
+
+    //-------- RETURN NODE TERAKHIR  --------//
+        template <class T>
+        Tree<T>* LastNode(Tree<T>* tr, int level, int tinggi){
+            if(tr == NULL){
+                return NULL;
+            }
+            if( level == tinggi){
+                return tr;
+            }
+
+            Tree<T>* a = LastNode(tr->kanan, level+1, tinggi);
+            Tree<T>* b = LastNode(tr->kiri, level+1, tinggi);
+            
+            if(a != NULL){
+                return a;
+            }
+            else if(b != NULL){
+                return b;
+            }
+            else{
+                return NULL;
+            }
+
+        }
+    //-------- ./RETURN NODE TERAKHIR --------//
+
+/*--------          ./PENCARIAN BINARY TREE          --------*/
 
 
 /*--------          TAMPILKAN BINARY TREE          --------*/
@@ -103,11 +193,81 @@ struct Tree{
                 }
             }
         }
-        //-------- ./INSERT DARI KIRI KE KANAN (KAYAK NULIS) [default] --------//
+    //-------- ./INSERT DARI KIRI KE KANAN (KAYAK NULIS) [default] --------//
 
+    //-------- INSERT BERDASARKAN KEY --------//
+        template <class T>
+        void insertOfKey(Tree<T>* tr, T key, T x){
+            Tree<T>* keyNode = searchNode(tr, key);
+            if(keyNode == NULL){
+                cout<<"Key tidak ditemukan!"<<endl;
+                return;
+            }
+
+            if(keyNode->kiri != NULL){
+                keyNode->kanan = new Tree<T>(x);
+                keyNode->kanan->pangkal = keyNode;
+            }
+            else{
+                keyNode->kiri = new Tree<T>(x);
+                keyNode->kanan->pangkal = keyNode;
+            }
+        }
+    //-------- ./INSERT BERDASARKAN KEY --------//
+    
 /*--------          ./TAMBAHKAN (INSERT) BINARY TREE          --------*/
 
+/*--------          HAPUS (DELETE) BINARY TREE          --------*/
 
+    //-------- HAPUS DARI POJOK KANAN BAWAH [default] --------//
+        template <class T>
+        void deleteDefault(Tree<T>* tr){
+            Tree<T>* hapus = LastNode(tr, 1, tinggiBT(tr));
+            if(hapus == NULL){
+                cout<<"Tree kosong!"<<endl;
+                return;
+            }
+            if(hapus->pangkal == NULL){
+                tr = NULL; delete tr;
+                return;
+            }
+
+            hapus = hapus->pangkal;
+            if(hapus->kanan == NULL){
+                hapus->kiri = NULL; delete hapus->kiri;
+            }
+            else{
+                hapus->kanan = NULL; delete hapus->kanan;
+            }
+        }
+    //-------- ./HAPUS DARI POJOK KANAN BAWAH [default] --------//
+
+    //-------- HAPUS BERDASARKAN KEY --------//
+        template <class T>
+        void deleteOfKey(Tree<T>* tr, T key){
+            Tree<T>* hapus = searchNode(tr, key);
+            if(hapus == NULL){
+                cout<<"Key tidak ditemukan!"<<endl;
+                return;
+            }
+            if(hapus->pangkal == NULL){
+                hapus = NULL; delete hapus;
+            }
+            else{
+                hapus = hapus->pangkal;
+                if(hapus->kanan->data == key){
+                    hapus->kanan = NULL; delete hapus->kanan;
+                }
+                else{
+                    hapus->kiri = NULL; delete hapus->kiri;
+                }
+            }
+
+        }
+    //-------- ./HAPUS BERDASARKAN KEY --------//
+
+
+/*--------          ./HAPUS (DELETE) BINARY TREE          --------*/
 
 int main(){
     Tree<int> *a = new Tree<int>(0);
@@ -116,9 +276,12 @@ int main(){
     insertDefault(a,3);
     insertDefault(a,4);
     insertDefault(a,5);
-    
+    insertDefault(a,6);
+    cout<<&a<<endl;
+    cout<<&a+1<<endl;
     cout<<"Preorder: "; preorder(a); cout<<endl;
     cout<<"Inorder: "; inorder(a); cout<<endl;
     cout<<"Postorder: "; postorder(a); cout<<endl;
-    cout<<"Tinggi BT: "<<tinggiBT(a,0)<<endl;
+    cout<<"Tinggi BT: "<<tinggiBT(a)<<endl;
+    
 }
